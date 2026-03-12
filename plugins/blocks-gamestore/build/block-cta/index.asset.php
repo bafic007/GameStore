10c48f60f7738f7e156110749bd4d63a091fbc4a
Roman.Lahoida <roman.lahoida@blago.group>
2026-03-12 19:59:28 +0200

    Add Call To Action Block

---
+++ plugins/blocks-gamestore/build/block-cta/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components'), 'version' => '396f2181eb464ff53f67');
---
+++ plugins/blocks-gamestore/src/block-cta/save.js
@@ -0,0 +1,41 @@
+import {RichText, useBlockProps} from '@wordpress/block-editor';
+
+export default function save({ attributes }) {
+	const { title, description, image, imageBg, links } = attributes;
+
+	return (
+		<div { ...useBlockProps.save({
+			className: "alignfull",
+			style: {
+				background: imageBg ? `url(${imageBg})` : undefined,
+			},
+		}) }>
+			<div className="wrapper cta-inner">
+				<div className="left-part">
+					<RichText.Content
+						tagName="h2"
+						className="cta-title"
+						value={title}
+					/>
+					<RichText.Content
+						tagName="p"
+						className="cta-description"
+						value={description}
+					/>
+					<div className="links-list">
+						{links.map((link, index) => (
+							<p key={index}>
+								<a href={link.url} target="_blank" rel="noopener noreferrer">
+									{link.anchor || "Untitled Link"}
+								</a>
+							</p>
+						))}
+					</div>
+				</div>
+				<div className="right-part">
+					{image && <img className="image-cta" src={image} alt="CTA" />}
+				</div>
+			</div>
+		</div>
+	);
+}
